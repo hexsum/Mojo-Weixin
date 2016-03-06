840c5d73d26e6f4e8d744028f798b02ba596f0af
sjdy521 <sjdy521@163.com>
2016-03-06 11:30:56 +0800

    Beauty插件可以自定义花瓣网画板地址，这样就可以按照自己喜欢的画板分类来发图

--- lib/Mojo/Weixin/Plugin/Beauty.pm
+++ lib/Mojo/Weixin/Plugin/Beauty.pm
@@ -1,22 +1,27 @@
 package Mojo::Weixin::Plugin::Beauty;
 our $PRIORITY = 95;
+use Storable qw(retrieve nstore);
 sub call{
     my $client = shift;
     my $data = shift;
-    my @pins;
-    my $last_pin_id;
     my $command = $data->{command} || '看妹子';
-    my $html = $client->http_get("http://huaban.com/favorite/beauty/");
-    if(not defined $html){
-        $self->error("插件[ ".__PACKAGE__ . " ]初始化数据失败");
-        return;
+    my $board = $data->{board} || 'http://huaban.com/boards/19570858/';
+    my $file = $data->{file} || './Beauty.dat';
+    my $db = {pins=>[],last_pin_id=>undef,board=>$board};
+    $db = retrieve($file) if -e $file;
+    if(defined $db->{board}  and $db->{board} ne $board){
+        $db = {pins=>[],last_pin_id=>undef,board=>$board};
     }
-    if($html=~m#\Qapp.page["pins"]\E\s*=\s*(.*);#){
-        my $json = $client->decode_json($1);
-        push @pins,{id=>$_->{pin_id}, url=>'http://img.hb.aicdn.com/' . $_->{file}{key}} for @$json;
+    #my $html = $client->http_get("http://huaban.com/favorite/beauty/");
+    my $html = $client->http_get($db->{board});
+    if(defined $html){
+        if($html=~m#\Qapp.page["board"]\E\s*=\s*(.*?);#){
+            my $json = $client->decode_json($1);
+            push @{$db->{pins}},{id=>$_->{pin_id}, url=>'http://img.hb.aicdn.com/' . $_->{file}{key}} for @{$json->{pins}};
+        }
     }
 
-    if(@pins == 0){
+    if(@{$db->{pins}} == 0){
         $self->error("插件[ ".__PACKAGE__ . " ]初始化数据失败");
         return;
     }
@@ -26,13 +31,13 @@ sub call{
         return if $msg->content ne $command; 
         return if $msg->from eq "bot";
         $msg->allow_plugin(0);
-        my $pin = shift @pins;
+        my $pin = shift @{$db->{pins}};
         if(not defined $pin){
             $self->http_get(
-                'http://huaban.com/favorite/beauty/?ilf1frwr&max='. $last_pin_id . '&limit=20&wfl=1',
+                $db->{board} . '?ilf1frwr&max='. $db->{last_pin_id} . '&limit=20&wfl=1',
                 {
                     Accept=>'application/json',
-                    Referer=>'http://huaban.com/favorite/beauty/',
+                    Referer=>$db->{board},
                     'X-Request'=>'JSON',
                     'X-Requested-With'=>'XMLHttpRequest',
                     json=>1,
@@ -40,8 +45,8 @@ sub call{
                 sub{
                     my $json = shift;
                     return if not defined $json;
-                    push @pins,{id=>$_->{pin_id}, url=>'http://img.hb.aicdn.com/' . $_->{file}{key}} for @{$json->{pins}};
-                    my $pin = shift @pins;
+                    push @{$db->{pins}},{id=>$_->{pin_id}, url=>'http://img.hb.aicdn.com/' . $_->{file}{key}} for @{$json->{pins}};
+                    my $pin = shift @{$db->{pins}};
                     if($msg->type eq "group_message"){
                         $client->send_media($msg->group,$pin->{url},sub{$_[1]->from("bot")});
                     }
@@ -51,7 +56,8 @@ sub call{
                     elsif($msg->type eq "friend_message" and $msg->class eq "send"){
                         $client->send_media($msg->receiver,$pin->{url},sub{$_[1]->from("bot")});
                     }
-                    $last_pin_id = $pin->{id};
+                    $db->{last_pin_id} = $pin->{id};
+                    nstore($db,$file);
                 }
             );
         }
@@ -65,7 +71,8 @@ sub call{
             elsif($msg->type eq "friend_message" and $msg->class eq "send"){
                 $client->send_media($msg->receiver,$pin->{url},sub{$_[1]->from("bot")});
             }
-            $last_pin_id = $pin->{id};
+            $db->{last_pin_id} = $pin->{id};
+            nstore($db,$file);
         }
     };
     $client->on(receive_message=>$callback,send_message=>$callback);
