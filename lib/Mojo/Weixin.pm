ced51e89209c9552b70ca3341a1bb1faa247cfa4
sjdy521 <sjdy521@163.com>
2016-03-15 11:15:04 +0800

    微信二维码图片扩展名改为jpg，感谢 @autodataming 反馈

--- lib/Mojo/Weixin.pm
+++ lib/Mojo/Weixin.pm
@@ -16,7 +16,7 @@ has log_encoding        => undef;      #utf8|gbk|...
 has tmpdir              => sub {File::Spec->tmpdir();};
 has media_dir           => sub {$_[0]->tmpdir};
 has cookie_dir          => sub {return $_[0]->tmpdir;};
-has qrcode_path         => sub {File::Spec->catfile($_[0]->tmpdir,join('','mojo_weixin_qrcode','.png'))};
+has qrcode_path         => sub {File::Spec->catfile($_[0]->tmpdir,join('','mojo_weixin_qrcode','.jpg'))};
 has ioloop              => sub {Mojo::IOLoop->singleton};
 has keep_cookie         => 1;
 
