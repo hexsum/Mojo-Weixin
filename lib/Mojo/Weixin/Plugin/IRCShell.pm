1156793b77b8b2d1a00ffd3951f76a315f50acf6
sjdy521 <sjdy521@163.com>
2017-02-10 23:32:06 +0800

    IRCShell bug修复

--- lib/Mojo/Weixin/Plugin/IRCShell.pm
+++ lib/Mojo/Weixin/Plugin/IRCShell.pm
@@ -270,7 +270,7 @@ sub call{
             if($data->{auto_join_channel}){
                 for(grep {$_->nick eq $master_irc_nick or $_->is_localhost}
                     grep {!$_->is_virtual} $ircd->users){
-                    $_->join_channel($channel);
+                    $_->join_channel($channel) if not $_->is_join_channel($channel);
                 }
             }
 
@@ -381,7 +381,7 @@ sub call{
             if($data->{auto_join_channel}){
                 for(grep {$_->nick eq $master_irc_nick or $_->is_localhost}
                     grep {!$_->is_virtual} $ircd->users){
-                    $_->join_channel($channel);
+                    $_->join_channel($channel) if not $_->is_join_channel($channel);
                 }
             }
 
