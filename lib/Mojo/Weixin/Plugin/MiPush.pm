6ff52f6932fcdd177b164f452836264323870cbb
周俊 <heipidage007@gmail.com>
2017-03-16 15:02:15 +0800

    Update MiPush.pm

--- lib/Mojo/Weixin/Plugin/MiPush.pm
+++ lib/Mojo/Weixin/Plugin/MiPush.pm
@@ -89,7 +89,7 @@ sub call {
         my($client,$event,@args) =@_;
         my $type = 'Mojo-Sys';
         my $message;
-        my $msgId = 1;
+        my $msgId = 2;
         my $title;
         if($event eq 'login'){
             $message = "登录成功";
@@ -100,7 +100,7 @@ sub call {
             $title = "扫描二维码事件";
         }
         elsif($event eq 'stop'){
-            $message = "Mojo-Webqq已停止";
+            $message = "Mojo-Weixin已停止";
             $title = "停止事件";
         }
         else{return}
